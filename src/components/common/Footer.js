46dbe40bcbfb00932f2e18c067aacf1ed3061591
Isaac-chip <15622125865@163.com>
2023-03-16 18:06:21 +0800

    初稿开发完成

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,6 +1,34 @@
 export default function Footer() {
   return (
     <footer className="p-5 bg-dark text-white text-center">
+      <div class="row">
+        <div class="row footer-widget">
+          <div
+            style={{ margin: "auto", width: "200px" }}
+            class="col-md-2 col-sm-6 col-md-offset-1 res-margin"
+          >
+            <h6>Useful Links</h6>
+            <div class="spce"></div>
+            <ul style={{ listStyle: "none", padding: "0" }}>
+              <li>
+                <a href="https://gcbuying.com/about-us">Home</a>
+              </li>
+              <li>
+                <a href="https://gcbuying.com/about-us">Contact Us</a>
+              </li>
+              <li>
+                <a href="https://gcbuying.com/privacy">Privacy Policy</a>
+              </li>
+             
+            </ul>
+          </div>
+        </div>
+        <div class="spce md"></div>
+        <div class="spce"></div>
+        <div class="copyright text-center light">
+          2021 Copyright <strong>Gcbuying</strong> . ALL RIGHTS RESERVED
+        </div>
+      </div>
       <div className="container">
         <p className="lead">
           Copyright &copy; 2023 Copyright GiftCardsng . ALL RIGHTS RESERVED
